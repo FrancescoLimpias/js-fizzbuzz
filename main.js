2060e7954ab466963fa832fe5d3b8a540d7999f2
Francesco Limpias <francescolimpias@gmail.com>
2022-11-09 15:59:39 +0100

    Bonus 1

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
+const log_view = document.getElementById("log");
+
 const breakInt = 100;
 const firstPrime = 3;
 const secondPrime = 5;
 
-for( let i = 1; i <= breakInt; i++){
-    if(i % firstPrime === 0){
-        console.log("Fizz");
-    } else if(i % secondPrime === 0){
-        console.log("Buzz");
-    } else if(i % (firstPrime * secondPrime) == 0){
-        console.log("FizzBuzz");
+for (let i = 1; i <= breakInt; i++) {
+    let result;
+    if (i % firstPrime === 0) {
+        result = "Fizz";
+    } else if (i % secondPrime === 0) {
+        result = "Buzz";
+    } else if (i % (firstPrime * secondPrime) == 0) {
+        result = "FizzBuzz";
     } else {
-        console.log(i);
+        result = i;;
     }
+
+    let resultDiv = document.createElement("div");
+    resultDiv.innerHTML = result;
+
+    log_view.append(resultDiv);
+
 }
